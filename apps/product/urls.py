154493a005313aaa22ae99dc87fc94cf6f642c3a
asafusuion-ux <asafusuion@gmail.com>
2026-05-02 18:16:20 +0600

    modifed/added - views/html/urls

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from apps.product.views import HomeView, TemplateView
+from apps.product.views import *
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('service/', ServiceView.as_view(), name='service'),
+    path('gallery/', GalleryView.as_view(), name='gallery'),
+    path('404/', ErrorView.as_view(), name='404'),
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -10,3 +10,30 @@ class HomeView(TemplateView):
         context['categories'] = Category.objects.all()
         context['products'] = Product.objects.all()
         return context
+
+class ContactView(TemplateView):
+    template_name = 'pages/contact.html'
+
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
+    
+class CheckoutView(TemplateView):
+    template_name = 'pages/checkout.html'
+
+class ShopView(TemplateView):
+    template_name = 'pages/shop.html'
+
+class ServiceView(TemplateView):
+    template_name = 'pages/service.html'
+
+class BlogView(TemplateView):
+    template_name = 'pages/blog.html'
+
+class CartView(TemplateView):
+    template_name = 'pages/cart.html'
+
+class GalleryView(TemplateView):
+    template_name = 'pages/gallery.html'
+
+class ErrorView(TemplateView):
+    template_name = 'pages/404.html'
